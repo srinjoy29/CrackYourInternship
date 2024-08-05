97ae07b72cf114caaf50f878473eccb8c8d217ff
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-08-05 11:00:57 +0530

    Update M - Find all duplicates in an array.java

--- Array/Day2/M - Find all duplicates in an array.java
+++ Array/Day2/M - Find all duplicates in an array.java
@@ -1,21 +1,29 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        if (nums.length == 1)
-        {
+        // If the input array contains only one element, return an empty list
+        if (nums.length == 1) {
             return new ArrayList<>();
         }
+
+        // Initialize an empty list to store the duplicates
         List<Integer> lst = new ArrayList<>();
+
+        // Create an array to count occurrences of each number in the input array
         int[] cnt = new int[nums.length + 1];
-        for (int num : nums)
-        {
+
+        // Count the occurrences of each number in the input array
+        for (int num : nums) {
             cnt[num]++;
         }
+
+        // Iterate through the count array to find numbers that appear exactly twice
         for (int i = 1; i < cnt.length; i++) {
-            if (cnt[i] == 2)
-            {
+            if (cnt[i] == 2) {
                 lst.add(i);
             }
         }
+
+        // Return the list of duplicates
         return lst;
     }
 }
