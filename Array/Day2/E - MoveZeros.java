ecb919dc8c289d8fac4574cd043054d08fe3fa70
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-08-05 13:09:23 +0530

    Update E - MoveZeros.java

--- Array/Day2/E - MoveZeros.java
+++ Array/Day2/E - MoveZeros.java
@@ -1,12 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // Initialize left pointer to 0
         int left = 0;
 
+        // Iterate through each element in the nums array with right pointer
         for (int right = 0; right < nums.length; right++) {
+            // If the current element is not zero, perform the swap
             if (nums[right] != 0) {
+                // Swap the elements at the right and left pointers
                 int temp = nums[right];
                 nums[right] = nums[left];
                 nums[left] = temp;
+                // Move the left pointer to the right
                 left++;
             }
         }        
